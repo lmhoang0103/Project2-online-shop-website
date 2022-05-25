a76f91139c17763c0fb4dc8e29e6e7bf1f125483
Le Minh Hoang <lmhoang0103@gmail.com>
2022-05-25 23:39:10 +0700

    add function

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,24 @@
-import React from 'react'
-import { Switch, Route } from 'react-router-dom'
-import Home from './pages/Home'
-
-import './App.css'
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import Home from "./pages/HomePage/Home";
+import Signup from "./pages/SignupPage/Signup"
+// import "./App.css";
+import ProductPage from "./pages/ProductPage/ProductPage";
 
 function App() {
-    return (
-        <Switch>
-            <Route path="/" component={Home} />
-        </Switch>
-    )
+  return (
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route path="/product/:id">
+        <ProductPage />
+      </Route>
+      <Route exact path= "/signup">
+        <Signup />
+      </Route>
+    </Switch>
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/components/Product/Product.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import imgShoe01 from "../../assets/images/shoe_01.jpg";
+import "./Product.css";
+
+const Product = ({ id, name }) => {
+  return (
+    <div className="product-container col-xl-6">
+      <Link to={`/product/${id}`}>
+        <img className="img-container" src={imgShoe01} alt="Error" />
+        <div className="info-container">
+          <div className="product-info">{name}</div>
+          <div className="seller-info">
+            <div className="product-price">
+              <span>20000</span>
+              <span className="units"> đ</span>
+            </div>
+            <div className="seller-amount">Sell: 5K</div>
+          </div>
+        </div>
+      </Link>
+    </div>
+  );
+};
+
+export default Product;
